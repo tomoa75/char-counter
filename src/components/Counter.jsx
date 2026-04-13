3a27025aefa172fff21d77d6a69f49bc7371d416
tomoa75 <84170526+tomoa75@users.noreply.github.com>
2026-04-13 18:31:01 +0200

    dovrseno css tune i responzivnost

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 export default function Counter({ text }) {
+  const [limit, setLimit] = useState(true);
+
   const counter = {};
   for (const char of text.toUpperCase()) {
     if (
@@ -23,23 +26,30 @@ export default function Counter({ text }) {
   ); //metoda za zbrajanje svih ponavljanja kako bismo mogli izračunati postotak
   const sortedCounter = Object.entries(counter).sort((a, b) => b[1] - a[1]); // Pretvaramo u niz parova i sortiramo po broju ponavljanja (od najčešćih do najrjeđih)
   const percentage = (count) => ((count / totalChars) * 100).toFixed(2); // Funkcija za izračunavanje postotka i formatiranje na 2 decimalna mjesta
-
+  const itemstoshow = limit ? sortedCounter.slice(0, 5) : sortedCounter; // Ovisno o stanju "limit", prikazujemo samo prvih 5 ili sve stavke
   return (
     <div className="counter">
       <h2>Letter Density</h2>
       <ul className="chart">
-        {sortedCounter.slice(0, 5).map(([char, count]) => (
+        {itemstoshow.map(([char, count]) => (
           <li key={char}>
-            {char}
-            <div
-              className="chartbar"
-              style={{ width: `${percentage(count)}%` }}
-            ></div>
-            {count} {percentage(count)}%
+            <div className="char">{char}</div>
+
+            <div className="chartbar">
+              <div
+                className="fill-bar"
+                style={{ "--percentage": `${percentage(count)}%` }}
+              ></div>
+            </div>
+            <div className="percentage">
+              {count} ({percentage(count)}%)
+            </div>
           </li>
         ))}
       </ul>
-      <button>See more ▽</button>
+      <button className="see-more" onClick={() => setLimit(!limit)}>
+        {limit ? "See more ▽" : "See less △"}
+      </button>
     </div>
   );
 }
